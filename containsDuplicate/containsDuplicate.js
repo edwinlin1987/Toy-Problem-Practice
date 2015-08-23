b3fd6e78265680d4951604a05fcdf1fc7415c6c1
Edwin Lin <edwinlin1987@gmail.com>
2015-08-23 13:09:12 -0700

    (

--- containsDuplicate/containsDuplicate.js
+++ containsDuplicate/containsDuplicate.js
@@ -4,7 +4,7 @@
  */
 var containsDuplicate = function(nums) {
   var dup = {};
-  for (var i = 0; i < nums.length ;i++) {
+  for (var i = 0; i < nums.length;i++) {
     if (dup[nums[i]] !== undefined) return true;
     else dup[nums[i]] = nums[i];   
   }
