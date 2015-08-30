46ef906c256751781c8232046adc58f14cb58714
Edwin Lin <edwinlin1987@gmail.com>
2015-08-30 00:55:54 -0700

    (toy) complete productExceptSelf

---
+++ productExceptSelf/productExceptSelf.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+  var result = [];
+  var result[nums.length-1] = 1;
+  
+  for (var i = nums.length-2; i >= 0; i--) {
+    result[i] = result[i+1] * nums[i+1];
+  }
+  
+  var current = 1;
+  for (i = 0; i < nums.length; i++) {
+    result[i] *= current;
+    current *= nums[i];
+  }
+
+  return result;
+};
